6206da5ad70ef3d24b9695be938d609180c91e4b
Farhan Tanvir <1805073@ugrad.cse.buet.ac.bd>
2023-01-31 00:20:03 +0600

    [ns2-off] time for submission

--- ns2-Offline/cmd.sh
+++ ns2-Offline/cmd.sh
@@ -9,19 +9,19 @@ baseline_area_side=500
 baseline_nodes=40
 baseline_flows=20
 
-echo "Area-Size(m/side)" > log.txt
-echo "Network-Throughtput(kilobits/sec) End-to-End-Avg-Delay(sec) Packet-Delivery-Ratio(%) Packet-Drop-Ratio(%)" >> log.txt
-echo "========== run.sh: varying area size =========="
+# echo "Area-Size(m/side)" > log.txt
+# echo "Network-Throughtput(kilobits/sec) End-to-End-Avg-Delay(sec) Packet-Delivery-Ratio(%) Packet-Drop-Ratio(%)" >> log.txt
+# echo "========== run.sh: varying area size =========="
 
-for((i=0; i<5; i++)); do
-    (( area_side = 250 + $i * 250 ))
-    echo $area_side >> log.txt
+# for((i=0; i<5; i++)); do
+#     (( area_side = 250 + $i * 250 ))
+#     echo $area_side >> log.txt
 
-    echo "offline.tcl: running with $area_side $baseline_nodes $baseline_flows"
-    ns offline.tcl $area_side $baseline_nodes $baseline_flows
-    echo "parse.awk: running"
-    awk -f parse.awk trace.tr >> log.txt 
-done
+#     echo "offline.tcl: running with $area_side $baseline_nodes $baseline_flows"
+#     ns offline.tcl $area_side $baseline_nodes $baseline_flows
+#     echo "parse.awk: running"
+#     awk -f parse.awk trace.tr >> log.txt 
+# done
 
 # echo "plot.py: running\n"
 # python plot.py
@@ -31,7 +31,7 @@ done
 # echo "========== run.sh: varying number of nodes =========="
 
 # for((i=0; i<5; i++)); do
-#     nodes=`expr 20 + $i \* 20`
+#     (( nodes = 20 + $i * 20 ))
 #     echo $nodes >> log.txt
 
 #     echo "offline.tcl: running with $baseline_area_side $nodes $baseline_flows"
@@ -43,19 +43,19 @@ done
 # echo "plot.py: running\n"
 # python plot.py
 
-# echo "Number-of-flows" > log.txt
-# echo "Network-Throughtput(kilobits/sec) End-to-End-Avg-Delay(sec) Packet-Delivery-Ratio(%) Packet-Drop-Ratio(%)" >> log.txt
-# echo "========== run.sh: varying number of flows =========="
+echo "Number-of-flows" > log.txt
+echo "Network-Throughtput(kilobits/sec) End-to-End-Avg-Delay(sec) Packet-Delivery-Ratio(%) Packet-Drop-Ratio(%)" >> log.txt
+echo "========== run.sh: varying number of flows =========="
 
-# for((i=0; i<5; i++)); do
-#     flows=`expr 10 + $i \* 10`
-#     echo $flows >> log.txt
+for((i=0; i<5; i++)); do
+    (( flows = 10 + $i * 10 ))
+    echo $flows >> log.txt
 
-#     echo "offline.tcl: running with $baseline_area_side $baseline_nodes $flows"
-#     ns offline.tcl $baseline_area_side $baseline_nodes $flows
-#     echo "parse.awk: running"
-#     awk -f parse.awk trace.tr >> log.txt
-# done
+    echo "offline.tcl: running with $baseline_area_side $baseline_nodes $flows"
+    ns offline.tcl $baseline_area_side $baseline_nodes $flows
+    echo "parse.awk: running"
+    awk -f parse.awk trace.tr >> log.txt
+done
 
 # echo "plot.py: running\n"
 # python plot.py
